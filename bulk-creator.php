48dda6e3a310d9cca37cb15b1756390283972cb8
Jeremy Wilson <asimovsghost@gmail.com>
2013-06-11 16:08:27 -0700

    Initial Commit

---
+++ bulk-creator.php
@@ -0,0 +1,327 @@
+<?php
+/*
+Plugin Name: Bulk Creator
+Plugin URI: http://capnjeremy.com
+Description: Allows bulk creation of posts, pages, and custom post types.
+Version: 1.0
+Author: Jeremy Wilson
+Author URI: http://capnjeremy.com
+License: WTFPL
+*/
+
+
+
+add_action( 'admin_menu', 'add_bp_pages' );
+
+
+
+function add_bp_pages(){
+    
+    
+    if ( current_user_can( 'manage_options' ) )  {
+    
+        add_utility_page( 'Bulk Creator', 'Bulk Creator', 'manage_options', 'bulk-creator', 'bulk_creator', plugin_dir_url().'bulk-creator/images/bulk-creator.png' );
+    
+    }
+
+
+}
+
+
+
+
+
+
+function bulk_creator() {
+
+
+
+    /* TODO
+    
+        -- Taxonomies
+        -- Post Meta? Maybe pull in from ACF?
+    
+    */
+
+
+    if ( !current_user_can( 'manage_options' ) )  {
+    
+    	wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+    
+    }
+    
+    // these are the default post types that are supported
+    $allowed_types = array(
+    
+        'post' => 'Posts',
+        'page' => 'Pages'
+    
+    );
+    
+    
+    // get a list of the custom post types
+    $custom_post_types = get_post_types( array('_builtin'=>false), 'objects' ); 
+    
+
+    // add the custom post types to the $allowed_types array
+    foreach($custom_post_types as $post_type){
+    
+    
+        $allowed_types[$post_type->name] = $post_type->labels->name;
+    
+    }
+    
+    
+    echo '<link rel="stylesheet" type="text/css" href="'.plugin_dir_url().'bulk-creator/css/bulk-creator.css">';
+    echo '<script src="'.plugin_dir_url().'bulk-creator/js/bulk-creator.js"></script>';
+    
+    
+    	
+	// begin main output
+	echo '<div class="wrap">';
+	
+	    
+	    ?>
+	    
+	        <div id="icon-edit" class="icon32 icon-bulk-creator"></div>
+	        
+
+	        <h2>Bulk Creator</h2>
+	        
+	        
+	        <nav class="post-types">
+	            
+	            <p><strong>Choose a Post Type</strong> 
+	            
+	            <?php
+	            
+	                foreach($allowed_types as $type=>$label) {
+	                
+	                    $active_class = "";
+	                    
+	                    if($_GET["post_type"] == $type) {
+	                    
+	                        $active_class = "active";
+	                    
+	                    }
+	                    
+	                    echo '<a class="button '.$active_class.'" href="?page='.$_GET['page'].'&post_type='.$type.'">'.$label.'</a>';
+	                
+	                }
+	            
+	            ?>
+	            
+	            </p>
+	            
+	        
+	        </nav>
+	        
+	        
+	        <?php
+	        
+	            if( isset($_POST['pages-list']) && $_POST['pages-list'] != '' ){
+	                 
+                	//form submitted
+                	if(preg_match_all('/(\d+\|(-|new)?\d+\|[^\n]*)/',$_POST['pages-list'],$match_pg)){
+                	
+                		$newpage = array();
+                		
+                		foreach($match_pg[0] as $pg_res){
+                		
+                			if(preg_match('/((\d+)\|((-|new)?\d+)\|(.*))/',$pg_res,$rres)){
+                			
+                				$parent = -1;
+                				
+                				if($rres[4]=='new'){
+                				
+                					$parent = $newpage[str_ireplace('new','',$rres[3])];
+                					
+                				}else{
+                				
+                					$parent = $rres[3];
+                					
+                				}
+                				
+                				if($parent==-1) $parent = 0;
+                				
+                				$pcontent = '';
+                				
+                				if($_POST['d-content'] != ''){
+                				
+                   					$_POST['d-content'] = str_ireplace('[pagetitle]','<h2>' . htmlentities($rres[5]) . '</h2>',$_POST['d-content']);
+                					
+                					$_POST['d-content'] = str_ireplace('[lipsum]', '<p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
+                					
+                					<h2>Header Level 2</h2>
+                						       
+                					<ol>
+                					   <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+                					   <li>Aliquam tincidunt mauris eu risus.</li>
+                					</ol>
+                					
+                					<blockquote><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. Ut a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis, tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.</p></blockquote>
+                					
+                					<h3>Header Level 3</h3>
+                					
+                					<ul>
+                					   <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+                					   <li>Aliquam tincidunt mauris eu risus.</li>
+                					</ul>', $_POST['d-content']);
+                					
+                					$pcontent = $_POST['d-content'];
+                					
+                				}
+                				
+                				$params = array( 
+                					'post_type' => $_GET["post_type"],
+                					'post_status' => 'publish',
+                					'post_parent' => $parent,
+                					'post_title' => rtrim($rres[5]),
+                					'post_content' => $pcontent);
+                				
+                				global $wpdb;
+                				
+                				$params['menu_order'] = $wpdb->get_var("SELECT MAX(menu_order)+1 AS menu_order FROM {$wpdb->posts} WHERE post_type='".$_GET["post_type"]."'");
+                				
+                				$wpdb->flush();
+                				
+                				$newpage[$rres[2]] = wp_insert_post($params);
+                			}
+                		} 
+                		
+                		echo '<script type="text/javascript">window.location=\'admin.php?page=bulk-creator&saved=1&post_type='.$_GET['post_type'].'\';</script>';
+                		
+                	} 
+                	
+                }
+	        
+	        if(isset($_GET['post_type']) && $_GET['post_type'] !='') { 
+	        
+	            $type = get_post_type_object($_GET["post_type"]);
+	        
+	        ?>
+	        
+	        <?php if(isset($_GET["saved"])) { ?>
+	        
+	            <div id="message" class="updated below-h2">
+	                <p><?php echo $type->labels->name; ?> Added!</p>
+	            </div>
+	        
+	        <?php } ?>
+	        
+	        <div id="poststuff">
+	        
+	            <form id="bulk-add" method="post" action="?page=<?php echo $_GET['page']; ?>&post_type=<?php echo $_GET['post_type']; ?>">
+	            
+	            <div class="add-page postbox">
+	                
+    	            <h3>Create New <span class="wpblue"><?php echo $type->labels->name; ?></span></h3>
+    	            
+    	            <div class="wrap">
+                            
+                            <label><strong><?php echo $type->labels->singular_name; ?> Title(s):</strong> 
+                                <textarea name="titles" id="titles" class="titles"></textarea>
+                                <span>Enter comma separated post titles (e.g.:  Post1, Post2, Post3)</span>
+                            </label>
+                            
+                            
+                            <?php
+                            
+                                if(is_post_type_hierarchical( $_GET['post_type'] )) {
+                            
+                            ?>
+                            
+                                <label>
+                                    <strong><?php echo $type->labels->singular_name; ?> Parent:</strong>
+                                    <?php wp_dropdown_pages('sort_column=menu_order&post_status=draft,publish&show_option_none=(No Parent)&post_type='.$_GET['post_type']); ?>
+                                    <span>Choose a parent for these <?php echo $type->labels->name; ?></span>
+                                </label>
+                                
+                                <br>
+                            
+                            <?php } ?>
+                            
+                            
+                            <label><strong>Default Content:</strong>
+                                <textarea name="d-content" id="d-content" class="d-content"></textarea>
+                                <span>Enter default content here.  Merge tags include: <abbr title="Inserts the page title wrapped in an h2">[pagetitle]</abbr>, <abbr title="Inserts a variety of dummy content">[lipsum]</abbr></span>
+                            </label>
+                            
+                            
+                            
+                            <textarea id="pages-list" name="pages-list" style="display:none;"></textarea>
+                            
+                            <input class="button button-large add-posts-button" type="button" id="add-pages" value="Add <?php echo $type->labels->name; ?>">
+                        
+                    </div>
+                
+                </div><!-- add-page -->
+                
+                
+                <div class="page-list postbox">
+                       
+                        <h3>Current <?php echo $type->labels->name; ?></h3>
+                    
+                        <div class="wrap">
+                    
+                            <ul>
+                            <?php
+                                
+                                if(is_post_type_hierarchical( $_GET['post_type'] )) {
+                                    
+                                    $args = array(
+                                        'title_li' => '',
+                                        'post_status' => 'publish,draft',
+                                        'echo' => 0,
+                                        'post_type' => $_GET['post_type']
+                                    
+                                    );
+                                        	                    
+                            	
+                            	    // strip links off of <li>s
+                                	echo preg_replace('/<a[^>]*>([^<]*)<\/a>/','\\1',wp_list_pages($args));
+                            	
+                            	
+                            	} else {
+                            	
+                                    $args = array(
+                                        'post_status' => 'publish,draft',
+                                        'post_type' => $_GET['post_type'],
+                                        'hierarchical' => '0'
+                                    );
+                                    
+                                	$post_list = get_posts($args);
+                            
+                                	foreach($post_list as $p) {
+                                	
+                                	    echo '<li class="page_item page-item-'.$p->ID.'">' . $p->post_title . '</li>';
+                                	
+                                	}
+                            
+                            	
+                            	}
+                            
+                                ?>
+                            
+                            </ul>
+                            
+                            <input type="submit" class="button button-primary button-large update-site" value="Apply Changes">
+                            
+                        </div>
+                    
+                
+                    </div><!-- page-list -->
+                    
+                    
+                    </form>
+                
+                
+	        </div><!-- poststuff -->
+	        
+        
+        <?php 
+        
+        } 
+	    
+	echo '</div>';
+}
---
+++ js/bulk-creator.js
@@ -0,0 +1,116 @@
+jQuery(document).ready(function(){
+
+    jQuery("#add-pages").click(function(){ add_pages(); });
+
+});
+    
+    var pageid = 0;
+    
+   function add_pages() {
+       
+       if(jQuery("#titles").val() == '') return false;
+       
+       jQuery(".update-site").css("visibility", "visible");
+        
+        // implement left-trim
+        String.prototype.ltrim = function() {
+        	return this.replace(/^\s+/,"");
+        }
+        
+        if(jQuery('#titles').val().match(/,/)) {
+        
+            // get and clean up page titles
+            var pageNames = jQuery('#titles').val().split(",");
+            
+            jQuery(pageNames).each(function(i){
+                pageNames[i]=this.ltrim();
+            });
+            
+            for(ipnames=0;ipnames<pageNames.length;ipnames++){
+            
+                jQuery('#titles').val(pageNames[ipnames]);
+                add_pages();
+                
+            }
+            
+            return false;
+        
+        } 
+        
+        
+        // get page parent ID (if set)
+        var parent = jQuery('#page_id').val();
+        
+        if(parent == '' || !parent) {
+        
+            parent = -1;
+            
+            jQuery('.page-list .wrap > ul').append('<li class="page-item-new' + pageid + '">' + jQuery('#titles').val() + ' <a href="JavaScript:del_page(' + pageid + ');">Remove</a></li>');
+        
+            jQuery('#page_id').append('<option value="new' + pageid + '">' + jQuery('#titles').val() + '</option>');
+        
+        } else {
+            
+            if(jQuery("#page_id").val()) {
+            
+                var parentname = jQuery('#page_id option[value=' + parent + ']').html();
+                
+            } else {
+            
+                var parentname = '';	                    
+                
+            }
+            
+            var parentspace = '&nbsp;&nbsp;&nbsp;';
+            
+            if(parentname.match(/&nbsp;/g)){
+            
+                var nums = parentname.match(/&nbsp;/g).length;
+                
+                for(inums=0;inums<nums;inums++){
+                    parentspace += '&nbsp;';
+                }
+                
+            }
+            
+            jQuery('.page-list .wrap > ul li.page-item-' + parent).append('<li class="page-item-new' + pageid + '">' + jQuery('#titles').val() + ' <a href="JavaScript:del_page(' + pageid + ');">Remove</a></li>');
+            
+            jQuery('#page_id option[value=' + parent + ']').after('<option class="p_' + parent + '" value="new' + pageid + '">' + parentspace + jQuery('#titles').val() + '</option>');
+            
+        
+        }
+        
+        
+        jQuery('#pages-list').val(jQuery('#pages-list').val() + pageid + '|' + parent + '|' + jQuery('#titles').val() + '\n');
+        
+        pageid++;
+        
+        jQuery('#titles').val('');
+        
+        jQuery('#page_id').attr('selectedIndex', 0);
+        
+        console.log(jQuery("#pages-list").val());
+        
+    }
+    
+    
+    
+    function del_page(pageid){
+    
+        jQuery('li.page-item-new' + pageid).remove();
+        
+        jQuery('#page_id option[value=new' + pageid + ']').remove();
+        
+        jQuery('#page_id option.p_new' + pageid).remove();
+        
+        var maintext = jQuery('#pages-list').val();
+        
+        //remove the page
+        maintext = maintext.replace(new RegExp(pageid + '\\|[^\\n]*\\n', "i"), "");
+        
+        //remove the children
+        maintext = maintext.replace(new RegExp('\\d*\\|new' + pageid + '\\|[^\\n]*\\n', "i"), "");
+        
+        //jQuery('#titles').val(maintext);
+        
+    }
